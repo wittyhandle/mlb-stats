cc0c6dcb38f74a63df8cfee91c7a71edd433ce69
Mike Ottinger <mike.ottinger@lithium.com>
2015-05-21 20:41:43 -0700

    got nodemon and browser-sync working together

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,21 +4,25 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var nodemon = require('gulp-nodemon');
 
-gulp.task('default', ['browser-sync'], function () {
-});
-
-gulp.task('browser-sync', ['nodemon'], function() {
-	browserSync.init(null, {
-		files: ["public/**/*.*"],
-		baseDir: "./",
-		debugInfo: false
+gulp.task('nodemon', function (cb) {
+	var called = false;
+	return nodemon({
+	  script: 'app.js'
+	}).on('start', function() {
+		if (!called) {
+      		called = true;
+      		cb();
+  		}
 	});
 });
 
-gulp.task('nodemon', function (cb) {
-	return nodemon({
-	  script: 'app.js'
-	}).on('start', function () {
-      cb();
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    files: ['public/**/*.*'],
+    baseDir: './',
+    debugInfo: false
   });
+});
+
+gulp.task('default', ['browser-sync'], function () {
 });
