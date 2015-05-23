5e579b5fa6aa57cc1f86d9c98879b2e22d7d69f7
Mike Ottinger <mike.ottinger@lithium.com>
2015-05-23 16:52:08 -0700

    missed files for sass task

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,10 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
 var nodemon = require('gulp-nodemon');
+
+var sass = require('gulp-sass');
 
 gulp.task('nodemon', function (cb) {
 	var called = false;
@@ -18,11 +21,24 @@ gulp.task('nodemon', function (cb) {
 
 gulp.task('browser-sync', ['nodemon'], function() {
   browserSync.init(null, {
-    files: ['public/**/*.*'],
     baseDir: './',
     debugInfo: false
   });
+
+  gulp.watch("public/sass/*.scss", ['sass']);
+  gulp.watch("routes/**/*.js").on('change', reload);
+
 });
+
+gulp.task('sass', function() {
+	gulp.src('public/sass/*.scss')
+	.pipe(sass({
+		errLogToConsole: true
+	}))
+	.pipe(gulp.dest('public/css/'))
+	.pipe(browserSync.stream());
+});
+
 
 gulp.task('default', ['browser-sync'], function () {
 });
