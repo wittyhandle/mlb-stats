7b65827911921b284db7d5615ffb5e5359cef0c9
Mike Ottinger <mike.ottinger@lithium.com>
2015-05-28 22:02:56 -0700

    store the token in session storage

--- public/js/auth/authService.js
+++ public/js/auth/authService.js
@@ -1,13 +1,14 @@
 'use strict';
 
-app.factory('authService', ['$q', '$http', function($q, $http) {
+app.factory('authService', ['$q', '$http', '$window', function($q, $http, $window) {
 
   var login = function(creds) {
 
     var deferred = $q.defer();
-    
-    $http.post('/login', creds)
+
+    $http.post('/api/authenticate', creds)
       .success(function(data, status, headers, config) {
+        $window.sessionStorage.token = data.token;
         deferred.resolve(data);
       })
       .error(function(data, status, headers, config) {
