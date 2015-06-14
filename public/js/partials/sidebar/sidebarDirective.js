33f12fbc8e43786302d2425fdd172ffb48fec5a2
Mike Ottinger <mike.ottinger@lithium.com>
2015-06-14 15:03:08 -0700

    render the sidebar menu from a directive

--- public/js/partials/sidebar/sidebarDirective.js
+++ public/js/partials/sidebar/sidebarDirective.js
@@ -1,6 +1,17 @@
 app.directive('cdgdSidebar', [function() {
   return {
     restrict: 'E',
-    templateUrl: 'js/partials/sidebar/sidebar-tpl.html'
+    templateUrl: 'js/partials/sidebar/sidebar-tpl.html',
+    link: function(scope, element, attrs) {
+
+      // these should come from some service
+      var menuItems = [
+        {label: 'Projects', id: 'projects'},
+        {label: 'Users', id: 'users'}
+      ];
+
+      scope.items = menuItems;
+
+    }
   };
 }]);
