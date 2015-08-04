e69a2c991b7e1de0cc490b66c5a40aba0a21a461
Mike Ottinger <mike.ottinger@lithium.com>
2015-08-04 09:15:03 -0700

    starting on user edit form

--- public/js/partials/users/userListDirective.js
+++ public/js/partials/users/userListDirective.js
@@ -1,4 +1,4 @@
-app.directive('cdgdUserList', ['userService', function(userService) {
+app.directive('cdgdUserList', ['userService', '$modal', function(userService, $modal) {
 
   return {
     restrict: 'E',
@@ -10,7 +10,34 @@ app.directive('cdgdUserList', ['userService', function(userService) {
       userService.getUsers().then(function(users) {
         scope.users = users;
       });
+
+      scope.editUser = function(user) {
+
+        var modalInstance = $modal.open({
+          animation: false,
+          templateUrl: 'js/partials/users/user-edit-tpl.html',
+          controller: 'editUserController',
+          size: 'lg',
+          resolve: {
+            user: function() {
+              return user;
+            }
+          }
+        });
+
+
+
+      }
+
     }
+  };
+
+}]).controller('editUserController', ['$scope', '$modalInstance', 'user', function($scope, $modalInstance, user) {
+
+  $scope.name = user.firstName + ' ' + user.lastName;
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
   };
 
 }]);
